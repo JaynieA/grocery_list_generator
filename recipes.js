688bb2985d0abbb771825885b0e2d220007624da
Jaynie <mrs.jaynie.anderson@gmail.com>
2016-11-08 15:04:43 -0600

    sorting by category

--- recipes.js
+++ recipes.js
@@ -8,10 +8,11 @@ var Recipe = function(name,servings){
   allMeals.push(this);
 }; //end Recipe
 
-var Ingredient = function(item, amount, measurement){
+var Ingredient = function(item, amount, measurement, category){
   this.item = item;
   this.amount = amount;
   this.measurement = measurement;
+  this.category = category;
 };//end Ingredient
 
 //PROTOTYPE FUNCTIONS
@@ -37,7 +38,7 @@ Recipe.prototype = {
 //OTHER FUNCTIONS
 var addAllIngredients = function(completeIngredientsArray, recipeObject){
   $.each(completeIngredientsArray, function(index, completeIngredientsArray) {
-    recipeObject.addIngredient(new Ingredient(completeIngredientsArray[0], completeIngredientsArray[1], completeIngredientsArray[2]));
+    recipeObject.addIngredient(new Ingredient(completeIngredientsArray[0], completeIngredientsArray[1], completeIngredientsArray[2], completeIngredientsArray[3]));
   });
 }; //end addAllIngredients
 
@@ -87,7 +88,7 @@ var printGroceryList= function(mealsArray){
       }
   });
   //Print formatted grocery list
-  //console.log("\n%cGrocery List: ", "font-size: large");
+  console.log("\n%cGrocery List: ", "font-size: large");
   for (var i = 0; i < output.length; i++) {
     if (output[i].amount === 0.5){
       output[i].amount = "1/2";
@@ -98,17 +99,39 @@ var printGroceryList= function(mealsArray){
     if (output[i].measurement === "item") {
       output[i].measurement = '';
     }
-    $('#groceryList').html($('#groceryList').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    //$('#groceryList').html($('#groceryList').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
     //console.log(output[i].amount+" "+output[i].measurement+" "+output[i].item);
+    if (output[i].category === "Produce") {
+      $('#produce').html($('#produce').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    }
+    if (output[i].category === "Frozen") {
+      $('#frozen').html($('#frozen').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    }
+    if (output[i].category === "Dairy") {
+      $('#dairy').html($('#dairy').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    }
+    if (output[i].category === "Meat") {
+      $('#meat').html($('#meat').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    }
+    if (output[i].category === "Other") {
+      $('#other').html($('#other').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    }
+    if (output[i].category === "Dry") {
+      $('#dry').html($('#dry').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    }
+    if (output[i].category === "Canned") {
+      $('#canned').html($('#canned').html()+"<li>"+ output[i].amount+" "+output[i].measurement+" "+output[i].item+"</li>");
+    }
+
   }
   return output;
 }; //end makeGroceryList
 
 var printWeeksMeals = function(mealsArray){
-  //console.log("\n%cThis week's meals: ", "font-size: large");
+  console.log("\n%cThis week's meals: ", "font-size: large");
   for (var i = 0; i < mealsArray.length; i++){
-    //console.log(mealsArray[i].name);
-    $('#meals').html($('#meals').html()+"<p>"+mealsArray[i].name+"</p>")
+    console.log(mealsArray[i].name);
+    $('#meals').html($('#meals').html()+"<p>"+mealsArray[i].name+"</p>");
   }
 }; //end printWeeksMeals
 
@@ -117,85 +140,85 @@ var printWeeksMeals = function(mealsArray){
 //Make Brown rice stir fry recipe
 var brownRiceStirFry = new Recipe("Brown Rice Stir Fry", 3);
 var stirFryIngredients = [
-  ["Brown Rice", 1, "Cup"],
-  ["Zucchini", 0.5, "item"],
-  ["Red Bell Pepper", 0.5, "item"],
-  ["Mushrooms", 5 , "oz"],
-  ["Soy Sauce", 3, "tbsp"],
-  ["Parsley", 2, "tbsp"],
-  ["Garlic", 4, "Cloves"],
-  ["Jalapeno", 0.5, "item"],
-  ["Cabbage", 1, "Cup"],
-  ["Broccoli", 0.5, "Head"],
-  ["Olive Oil", 1, "tbsp"]
+  ["Brown Rice", 1, "Cup", "Dry"],
+  ["Zucchini", 0.5, "item", "Produce"],
+  ["Red Bell Pepper", 0.5, "item", "Produce"],
+  ["Mushrooms", 5 , "oz", "Produce"],
+  ["Soy Sauce", 3, "tbsp", "Other"],
+  ["Parsley", 2, "tbsp", "Produce"],
+  ["Garlic", 4, "Cloves", "Produce"],
+  ["Jalapeno", 0.5, "item", "Produce"],
+  ["Cabbage", 1, "Cup", "Produce"],
+  ["Broccoli", 0.5, "Head", "Produce"],
+  ["Olive Oil", 1, "tbsp", "Other"]
 ];
 addAllIngredients(stirFryIngredients, brownRiceStirFry);
 
 //Make One Pan Mexican Quinoa recipe
 var onePanMexQuinoa = new Recipe("One Pan Mexican Quinoa", 4);
 var mexQuinoaIngredients = [
-  ["Olive Oil", 1, "tbsp"],
-  ["Garlic", 2, "Cloves"],
-  ["Jalapeno", 1, "item"],
-  ["Quinoa", 1, "Cup"],
-  ["Vegetable Broth", 1, "Cup"],
-  ["Black Beans", 1, "15 oz. Can"],
-  ["Fire Roasted Diced Tomatoes", 1, "14.5 oz Can"],
-  ["Corn Kernels", 1, "Cup"],
-  ["Chili Powder", 1, "tsp"],
-  ["Cumin", 0.5, "tsp"],
-  ["Lime", 1, "item"],
-  ["Cilantro", 2, "tbsp"]
+  ["Olive Oil", 1, "tbsp", "Other"],
+  ["Garlic", 2, "Cloves", "Produce"],
+  ["Jalapeno", 1, "item", "Produce"],
+  ["Quinoa", 1, "Cup", "Dry"],
+  ["Vegetable Broth", 1, "Cup", "Canned"],
+  ["Black Beans", 1, "15 oz. Can", "Canned"],
+  ["Fire Roasted Diced Tomatoes", 1, "14.5 oz Can", "Canned"],
+  ["Corn Kernels", 1, "Cup", "Canned"],
+  ["Chili Powder", 1, "tsp", "Other"],
+  ["Cumin", 0.5, "tsp", "Other"],
+  ["Lime", 1, "item", "Produce"],
+  ["Cilantro", 2, "tbsp", "Produce"]
 ];
 addAllIngredients(mexQuinoaIngredients, onePanMexQuinoa);
 
 //Make Pizza Recipe
 var pizza = new Recipe("Pizza", 2);
 var pizzaIngredients = [
-    ["Pizza Crusts",1,"package"],
-    ['Cheese',1,"Cup"],
-    ['Marinara Sauce', 0.5, "Cup"]
+    ["Pizza Crusts",1,"package", "Dry"],
+    ['Cheese',1,"Cup", "Dairy"],
+    ['Marinara Sauce', 0.5, "Cup", "Canned"]
 ];
 addAllIngredients(pizzaIngredients, pizza);
 
 //make mac n' cheese recipe
 var macNcheese = new Recipe("Mac and Cheese and Green Beans", 2);
 var macNcheeseIngredients = [
-  ["Macaroni and Cheese", 1, "Box"],
-  ["Milk", 2, "tbsp"],
-  ["Shredded Cheese", 0.25, "Cup"],
-  ["Butter",1, "tbsp"],
-  ["Parmesan Cheese", 0.5, "Cup"],
-  ["Green Beans", 1, "Package"]
+  ["Macaroni and Cheese", 1, "Box", "Dry"],
+  ["Milk", 2, "tbsp", "Dairy"],
+  ["Shredded Cheese", 0.25, "Cup", "Dairy"],
+  ["Butter",1, "tbsp", "Dairy"],
+  ["Parmesan Cheese", 0.5, "Cup", "Dairy"],
+  ["Green Beans", 1, "Package", "Produce"]
 ];
 addAllIngredients(macNcheeseIngredients, macNcheese);
 
 //make Spicy Thai Noodles Recipe
 var spicyThaiNoodles = new Recipe("One Pot Spicy Thai Noodles", 4);
 var spicyThaiNoodlesIngredients = [
-  ["Linguine Noodles", 1,"Lb"],
-  ["Olive Oil", 2, "tbsp"],
-  ["Eggs", 2, "item"],
-  ["Crushed Red Pepper Flakes", 0.5, "tsp"],
-  ["Zucchini", 1, "item"],
-  ["Mushrooms", 10, "oz"],
-  ["Garlic", 3, "Cloves"],
-  ["Brown Sugar", 2, "tbsp"],
-  ["Soy Sauce", 6, "tbsp"],
-  ["Sriracha", 1, "tbsp"],
-  ["Ginger Root", 2, "inches"],
-  ["Cilantro", 1, "handful"],
-  ["Green Onions", 4, "item"]
+  ["Linguine Noodles", 1,"Lb", "Dry"],
+  ["Olive Oil", 2, "tbsp", "Other"],
+  ["Eggs", 2, "item", "Dairy"],
+  ["Crushed Red Pepper Flakes", 0.5, "tsp", "Other"],
+  ["Zucchini", 1, "item", "Produce"],
+  ["Mushrooms", 10, "oz", "Produce"],
+  ["Garlic", 3, "Cloves", "Produce"],
+  ["Brown Sugar", 2, "tbsp", "Dry"],
+  ["Soy Sauce", 6, "tbsp", "Other"],
+  ["Sriracha", 1, "tbsp", "Other"],
+  ["Ginger Root", 2, "inches", "Produce"],
+  ["Cilantro", 1, "handful", "Produce"],
+  ["Green Onions", 4, "item", "Produce"]
 ];
 addAllIngredients(spicyThaiNoodlesIngredients, spicyThaiNoodles);
 
 //make gnocci and green beans Recipe
 var gnocciAndGreenBeans = new Recipe("Sweet Potato Gnocci and Parmesan Green Beans", 2);
 var gnocciAndBeansIngredients = [
-  ["Sweet Potato Gnocci", 1, "Frozen Package"],
-  ["Green Beans", 1, "Package"],
-  ["Parmesan", 0.25, "Cup"],
-  ["Butter", 0.5, "tbsp"]
+  ["Sweet Potato Gnocci", 1, "Frozen Package", "Frozen"],
+  ["Green Beans", 1, "Package", "Produce"],
+  ["Parmesan", 0.25, "Cup", "Dairy"],
+  ["Butter", 0.5, "tbsp", "Dairy"]
 ];
 addAllIngredients(gnocciAndBeansIngredients, gnocciAndGreenBeans);
 
